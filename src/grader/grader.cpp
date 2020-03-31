5f7724f268a5c8114ad2255e00d8d0eb114d3fba
Mariusz Rozycki <mar.rozycki@gmail.com>
2020-03-31 10:14:56 +0200

    Extract test data loader to a separate file

--- src/grader/grader.cpp
+++ src/grader/grader.cpp
@@ -6,40 +6,11 @@
 #include <spdlog/spdlog.h>
 
 #include "system/process.h"
+#include "test_loader.h"
 
 namespace fs = boost::filesystem;
 
 namespace {
-struct test {
-	std::size_t id;
-	fs::path input_path;
-	fs::path output_path;
-	int memory_limit;
-	int time_limit;
-};
-
-std::vector<test> load_tests(fs::path const& test_path) {
-	std::fstream test_config(test_path / "tests");
-	if (!test_config) {
-		throw std::runtime_error("Cannot open test config file");
-	}
-
-	std::size_t test_count;
-	test_config >> test_count;
-
-	std::vector<test> tests;
-	tests.reserve(test_count);
-	for (auto i = 0; i < test_count; i++) {
-		auto& test = tests.emplace_back();
-		test.id = i+1;
-		std::string input_filename, output_filename;
-		test_config >> input_filename >> output_filename >> test.memory_limit >> test.time_limit;
-		test.input_path = test_path / input_filename;
-		test.output_path = test_path / output_filename;
-	}
-	return tests;
-}
-
 enum grade {
     TIMEOUT = 0,
     TIME_EXCEEDED = 3,
@@ -54,7 +25,7 @@ namespace ugg {
 int grade(std::string_view problem_id, fs::path const& executable_path) {
 	std::vector<test> tests;
 	try {
-		tests = load_tests(fs::path("problems") / std::string(problem_id));
+		tests = test_loader(fs::path("problems")).load_tests(problem_id);
 	} catch (std::exception const& e) {
 		spdlog::error("Failed to load test config: {}", e.what());
 		return -1;
---
+++ src/grader/test_loader.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <boost/filesystem.hpp>
+
+namespace ugg {
+struct test {
+	std::size_t id;
+	boost::filesystem::path input_path;
+	boost::filesystem::path output_path;
+	int memory_limit;
+	int time_limit;
+};
+
+class test_loader {
+public:
+    test_loader(boost::filesystem::path data_path) : data_path_(std::move(data_path)) {}
+
+    std::vector<test> load_tests(std::string_view problem_id) const;
+
+private:
+    boost::filesystem::path data_path_;
+};
+}
