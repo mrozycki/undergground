eb4408a27a0f4b84df5fd292cce09ce630e8cd4c
Mariusz Rozycki <mar.rozycki@gmail.com>
2020-04-04 12:37:00 +0200

    Store compiled executables in a temporary location
    fixes #7

--- src/grader/compiler.cpp
+++ src/grader/compiler.cpp
@@ -14,12 +14,14 @@ compiler::compiler(boost::filesystem::path const& compiler_path)
 std::optional<boost::filesystem::path> compiler::compile(boost::filesystem::path const& source_file) {
     spdlog::info("Compiling file: {}", source_file.native());
 
+    auto output_path = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path();
+    spdlog::info("Temporary file path: {}", output_path.native());
     auto compiler_process = ugg::system::start_process(
-        compiler_path_, {"-O2", "-Wall", source_file.c_str(), "-o", "solution"});
+        compiler_path_, {"-O2", "-Wall", source_file.c_str(), "-o", output_path.c_str()});
 
     if (compiler_process.err().getc() == EOF) {
         spdlog::info("Compilation suceeded");
-        return {"solution"};
+        return {output_path};
     } else {
         spdlog::info("Compilation failed");
         compiler_process.kill();
