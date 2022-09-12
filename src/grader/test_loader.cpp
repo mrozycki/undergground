ed7df0381d919166b12e915a813a8073c1d8c237
Mariusz Różycki <mar.rozycki@gmail.com>
2022-09-12 20:29:54 +0200

    Add support for YAML test config files

--- src/grader/test_loader.cpp
+++ src/grader/test_loader.cpp
@@ -1,6 +1,7 @@
 #include "grader/test_loader.h"
 
 #include <spdlog/spdlog.h>
+#include <yaml-cpp/yaml.h>
 
 namespace fs = boost::filesystem;
 
@@ -8,23 +9,19 @@ namespace ugg {
 std::vector<test> test_loader::load_tests(std::string_view problem_id) const {
     auto test_path = data_path_ / problem_id.data();
     spdlog::info("Loading test config from {}", fs::absolute(test_path).native());
-    std::fstream test_config(test_path / "tests");
-    if (!test_config) {
-        throw std::runtime_error("Cannot open test config file");
-    }
 
-    std::size_t test_count;
-    test_config >> test_count;
-
+    auto const config = YAML::LoadFile((test_path / "tests.yaml").string());
     std::vector<test> tests;
-    tests.reserve(test_count);
-    for (std::size_t i = 0; i < test_count; i++) {
+    tests.reserve(config["tests"].size());
+
+    std::size_t i = 0;
+    for (auto const& test_config : config["tests"]) {
         auto& test = tests.emplace_back();
-        test.id = i + 1;
-        std::string input_filename, output_filename;
-        test_config >> input_filename >> output_filename >> test.memory_limit >> test.time_limit;
-        test.input_path = test_path / input_filename;
-        test.output_path = test_path / output_filename;
+        test.id = ++i;
+        test.input_path = test_path / test_config["input_file"].as<std::string>();
+        test.output_path = test_path / test_config["output_file"].as<std::string>();
+        test.memory_limit = test_config["memory_limit"].as<int>();
+        test.time_limit = test_config["time_limit"].as<int>();
     }
     return tests;
 }
